0b9168d4a72a7950ad4111c690900e4d0d70983f
juanp <juan.tovar@epn.edu.ec>
2021-07-25 15:02:16 -0500

    Refactorización extract Method en la clase de pruebas AutorizacionInventarioTest

--- src/test/java/GestiónDeInventario/AutorizacionInventarioTest.java
+++ src/test/java/GestiónDeInventario/AutorizacionInventarioTest.java
@@ -1,5 +1,6 @@
 package GestiónDeInventario;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -7,15 +8,20 @@ import static org.junit.Assert.*;
 
 public class AutorizacionInventarioTest {
 
+    AutorizacionInventario autorizacion=null;
+
+    @Before
+    public void setup(){
+        autorizacion = Mockito.mock(AutorizacionInventario.class);
+    }
+
     @Test
     public void when_solicitarAutorizacion_then_ok() {
-        AutorizacionInventario autorizacion = Mockito.mock(AutorizacionInventario.class);
         Mockito.when(autorizacion.solicitarAutorizacion()).thenReturn(true);
         assertTrue(autorizacion.solicitarAutorizacion());
     }
     @Test
     public void when_solicitarAutorizacion_then_false() {
-        AutorizacionInventario autorizacion = Mockito.mock(AutorizacionInventario.class);
         Mockito.when(autorizacion.solicitarAutorizacion()).thenReturn(false);
         assertFalse(autorizacion.solicitarAutorizacion());
     }
