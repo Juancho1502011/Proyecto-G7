ff5a4cc26b2c48f7c0560466d34bb362feb0b479
juanp <juan.tovar@epn.edu.ec>
2021-07-25 15:57:19 -0500

    Implementación del método disminuirInventario con TDD

--- src/main/java/GestiónDeInventario/Inventario.java
+++ src/main/java/GestiónDeInventario/Inventario.java
@@ -13,4 +13,15 @@ public class Inventario {
         a.setCantidad(a.getCantidad() + cantidad);
         return a.getCantidad();
     }
+
+    public int disminuirInventario(Auto auto, int cantidad) {
+        Auto a1 = autos.buscarAutosComunes(auto);
+        if(a1.getCantidad()<cantidad){
+            System.out.println("No existe el número indicado en stock");
+            return a1.getCantidad();
+        }else{
+            a1.setCantidad(a1.getCantidad() - cantidad);
+        }
+        return a1.getCantidad();
+    }
 }
--- src/test/java/GestiónDeInventario/InventarioTest.java
+++ src/test/java/GestiónDeInventario/InventarioTest.java
@@ -19,4 +19,16 @@ public class InventarioTest {
                 new Auto("Chevrolet","Grand Vitara",2021, "rojo"), cantidad);
         assertEquals(5, actual);
     }
+
+    @Test
+    public void given_auto_when_disminuirInventario_then_ok(){
+        Inventario inventario = new Inventario();
+        int actual = inventario.aumentarInventario(new Auto("Chevrolet","Grand Vitara",2021, "rojo"), 5);
+        assertEquals(2,inventario.disminuirInventario(new Auto("Chevrolet","Grand Vitara",2021, "rojo"),3));
+    }
+    @Test
+    public void given_auto_when_disminuirInventario_then_error(){
+        Inventario inventario = new Inventario();
+        assertEquals (0,inventario.disminuirInventario(new Auto("Chevrolet","Grand Vitara",2021, "rojo"),3));
+    }
 }
