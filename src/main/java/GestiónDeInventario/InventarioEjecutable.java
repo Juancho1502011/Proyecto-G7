76fbb6b90af0ad666621ea02c50a91755fede49f
Peterca <pierrecadeaubrutus@gmail.com>
2021-07-25 15:40:54 -0500

    Actualizacion del main y del archivo JAR  con los nuevos cambios.

--- src/main/java/GestiónDeInventario/InventarioEjecutable.java
+++ src/main/java/GestiónDeInventario/InventarioEjecutable.java
@@ -6,5 +6,11 @@ public class InventarioEjecutable {
         Inventario inventario = new Inventario();
         System.out.println("*******Ejecución del método mostrarInventario");
         System.out.println(inventario.autos.mostrarInventario());
+
+       //Aumentando el inventario
+        System.out.println("\n");
+        inventario.aumentarInventario(new Auto("Chevrolet","Grand Vitara",2021, "rojo"), 5);
+        System.out.println("=====Mostrando el nuevo inventario=====");
+        System.out.println(inventario.autos.mostrarInventario());
     }
 }
